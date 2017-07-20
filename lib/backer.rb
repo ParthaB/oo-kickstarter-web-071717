1579822c0ee785ef1ab64824f703a48d9ca995c5
Prasenjit Banik <pbp2005@gmail.com>
2017-07-20 19:30:14 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+#Zali
+#Dean
+class Backer
+
+  attr_accessor :backed_projects, :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project_obj)
+    backed_projects << project_obj
+    project_obj.backers << self
+  end
+
+end
